fa0039bfa0b55ec5488b99b2febb53e87157e9ae
clintonjosephs <clintonjosephs26@gmail.com>
2021-12-20 13:05:23 +0100

    Fixed linters errors

--- assets/js/program.js
+++ assets/js/program.js
@@ -1,33 +1,33 @@
 const programs = [
-                    {
-                      icon: '<i class="fa fa-users fa-2x"></i>',
-                      title: 'Network',
-                      details: 'Network and interact with top industry individuals with many years of experience.',
-                    },
+  {
+    icon: '<i class="fa fa-users fa-2x"></i>',
+    title: 'Network',
+    details: 'Network and interact with top industry individuals with many years of experience.',
+  },
 
-                    {
-                      icon: '<i class="fa fa-rocket fa-2x"></i>',
-                      title: 'Workshops',
-                      details: 'Software exhibitions, idea box, hackerton, code fest with so many prices to be won',
-                    },
+  {
+    icon: '<i class="fa fa-rocket fa-2x"></i>',
+    title: 'Workshops',
+    details: 'Software exhibitions, idea box, hackerton, code fest with so many prices to be won',
+  },
 
-                    {
-                      icon: '<i class="fa fa-comments-o fa-2x"></i>',
-                      title: 'Forum',
-                      details: 'Get opportunity to join forum of great minds, test your intellectual prowess',
-                    },
+  {
+    icon: '<i class="fa fa-comments-o fa-2x"></i>',
+    title: 'Forum',
+    details: 'Get opportunity to join forum of great minds, test your intellectual prowess',
+  },
 
-                    {
-                      icon: '<i class="fa fa-microphone fa-2x"></i>',
-                      title: 'Talks',
-                      details: 'Lets talk about security, software development models,  and so much more',
-                    },
-                    {
-                      icon: '<i class="fa fa-graduation-cap fa-2x"></i>',
-                      title: 'Certifications',
-                      details: 'Certificate of participation will be given out to all those in attendance',
-                    }
-                  ];
+  {
+    icon: '<i class="fa fa-microphone fa-2x"></i>',
+    title: 'Talks',
+    details: 'Lets talk about security, software development models,  and so much more',
+  },
+  {
+    icon: '<i class="fa fa-graduation-cap fa-2x"></i>',
+    title: 'Certifications',
+    details: 'Certificate of participation will be given out to all those in attendance',
+  },
+];
 
 function getPrograms(program) {
   const programData = ` <div class="program-item">
@@ -48,7 +48,7 @@ const programsSection = document.querySelector('.program-main');
 
 function showPrograms() {
   let programsBuild = '';
-  programs.forEach(program => {
+  programs.forEach((program) => {
     programsBuild += getPrograms(program);
   });
   programsSection.innerHTML = programsBuild;
